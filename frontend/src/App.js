9da26129613f40aac25658a4f3a10c9cd56b66c9
Tobias <tobi.l@posteo.at>
2020-03-21 22:28:25 +0100

    feat(app): Add meta information and assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Pub } from './pages/pub'
+import { Helmet } from "react-helmet";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
+import { Pub } from './pages/pub'
 import { Home } from './pages/home'
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>skei.pe</title>
+        <link rel="icon" type="image/png" href="(GENAUE PFADANGABE/DATEINAME.png"></link>
+      </Helmet>
       <Router>
         <Switch>
           <Route path="/pub/:pubName">
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { MapBox } from '../components/map'
-import { Helmet } from "react-helmet";
 import { Nav } from '../components/nav'
 
 const Home = () => {
   return (
     <>
-      <Helmet>
-        <title>skei.pe</title>
-      </Helmet>
       <Nav />
       <div>
         <MapBox />
